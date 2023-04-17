c059c08ed86e08ea1a4f90840e716847dad42b8d
xinyue <maxy@cloudscope.cn>
2023-04-17 17:12:54 +0800

    ✨ feat(ssr pre-rendering gray-matter getStaticProps):

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>hello this is ma xinyue. </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
