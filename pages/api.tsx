5d5b8b06ef862691af0753ed7f702f825576b062
xinyue <maxy@cloudscope.cn>
2023-04-24 13:59:43 +0800

    ✨ feat(handler api route):

--- pages/api.tsx
+++ pages/api.tsx
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req: NextApiRequest, res: NextApiResponse) {
+export default function handler (req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json({ text: 'Hello world' });
 }
