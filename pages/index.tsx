8556d06483df7e2b723ad892f9e20b2201fdce23
xinyue <maxy@cloudscope.cn>
2023-04-26 17:45:32 +0800

    暗夜模式

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,7 +60,7 @@ export default function Home({
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         {/* <h2 className={utilStyles.headingLg}>Blog</h2> */}
-        <ul className={utilStyles.list}>
+        <ul className={`${utilStyles.list} dark:text-dark`}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
